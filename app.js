3368866cc5d29e8e64a132b7802e065aa1e194c8
drkilla <sa.stanojevicaleksandar@gmail.com>
2021-06-08 02:05:14 +0200

    mise en place des component

--- app.js
+++ app.js
@@ -1,20 +1,86 @@
-let n = 0
-function numberFormat(n){
-    return n.toString().padStart(2,'0')
-}
-function render(){
-    const title = <h1>
-        Bonjour les gens <span>{n}</span>
-        </h1>
-
-
-ReactDOM.render(title,document.querySelector('#app'))
-
+function WelcomeFunc({name,children}) {
+    return <div>
+        <h1>Bonjour {name}</h1>
+    <p>
+        {children}
+    </p>
+    </div>
 }
 
-render()
+class Welcome extends React.Component{
+    render(){
+        return <div>
+        <h1>Bonjour {this.props.name}</h1>
+    <p>
+        {this.props.children}
+    </p>
+    </div>
+        
+    }
+}
 
-window.setInterval(() => {
-    n++
-    render()
-},1000)
+class Clock extends React.Component {
+
+    constructor (props){
+        super(props)
+        this.state = {date : new Date()}
+        this.timer = null 
+    }
+
+    componentDidMount()
+    {
+        this.timer = window.setInterval(this.tick.bind(this),1000)
+    }
+
+    componentwillUnmount()
+    {
+        window.clearInterval(this.timer)
+    }
+
+    tick(){
+        this.setState({date: new Date()})
+    }
+
+    render() {
+        const date = new Date()
+        return (
+             <div>
+                 Il est {this.state.date.toLocaleDateString()} {this.state.date.toLocaleTimeString()}
+             </div>
+        );
+    }
+}
+class Incrementer extends React.Component {
+    constructor (props)
+    {
+        super(props)
+        this.state = {n : props.start}
+        this.timer = null
+    }
+    componentDidMount () {
+        window.setInterval(this.increment.bind(this),1000)
+    }
+    componentwillUnmount () {
+        window.clearInterval(this.timer)
+    }
+    increment(){
+        this.setState((state,props)=>({n: state.n + 1}))
+    }
+    render(){
+        return <div> Valeur : {this.state.n}</div>
+    }
+
+}
+
+function Home(){
+    return <div>
+        <Welcome name="Dorothée" />
+        <Welcome name="Jean" />
+        <Clock></Clock>
+        <Incrementer start={10}></Incrementer>
+
+    </div>
+}
+
+ReactDOM.render(<Home></Home>, document.querySelector('#app'))
+ 
